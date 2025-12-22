71ce22120eb9e5434eff8b36dddcc40eba0ae81f
Oliver Killane <oliverkillane@gmail.com>
2025-12-22 22:37:46 +0100

    using fixture macro

---
+++ src/derive-cpp/test/gtest_mock.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#define FIXTURE_MOCK(fixture, ret, name, ...)                                                      \
+    MOCK_METHOD(ret, name##_mock, __VA_ARGS__);                                                    \
+    derivecpp::Trampoline<&name, &fixture::name##_mock> name##_tramp{this};
